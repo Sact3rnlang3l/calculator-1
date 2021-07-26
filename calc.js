82921f9339d30d7005723b45ff08f83e0e5a87ce
Aiden gillespie <aiden@cold.org>
2021-07-26 15:56:58 -0600

    Updated File with Added functionality for exponent, and modulus or remainder

--- calc.js
+++ calc.js
@@ -1,5 +1,7 @@
+const { Console } = require('console');
 const { read } = require('fs');
 const readline = require('readline');
+const { markAsUntransferable } = require('worker_threads');
 
 const reader = readline.createInterface({
   input: process.stdin,
@@ -21,10 +23,14 @@ reader.question("What would you like to calculate?", function(input){
 		else if (mathSymbol === "*")
 			console.log(num1 * num2)
 		else if (mathSymbol === "/")
-			console.log(num1 / num2)
+			console.log(num1/num2)
 		else if (mathSymbol === "√")
 			console.log(Math. sqrt(num1))
-
+		else if (mathSymbol=== "**")
+			console.log(num1**num2)
+			else if (mathSymbol === "%")
+			console.log(num1%num2)
+			
 	
 
 	// This line closes the connection to the command line interface.
